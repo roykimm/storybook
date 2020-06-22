b3ccb4ba611d7c128477ceba72bcc10a88d51926
roykimm <roykimmyunghoon@gmail.com>
2020-06-23 00:45:27 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db');
+
+// Load config
+dotenv.config({ path : './config/config.env' });
+
+connectDB();
+
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT, 
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
